3a5be7565dc645614e08e1d0fac3d895be633e5d
Ilikecurry30 <Jayanthkjohnson@gmail.com>
2026-01-22 12:44:14 -0500

    Update Duplicate.java

--- Duplicate.java
+++ Duplicate.java
@@ -1,16 +1,18 @@
 public class Duplicate {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         String[] classes = { "Algebra 2", "Physics", "Physics", "DDP", "Lunch", "APCSA", "Study Hall", "English 11R", "U.S. History" };
         System.out.println("Contains duplicates: " + containsDuplicate(classes));
     }
 
-    public static boolean containsDuplicate (String[] classes){
-        for (int i = 0; i < classes.length; i++){
-            for (int j = i + 1; j < classes.length; j++){
-                if (classes[i].equals(classes[j])){
+    public static boolean containsDuplicate(String[] classes) {
+        int i = 0;
+        while (i < classes.length - 1) {
+            for (int j = i + 1; j < classes.length; j++) {
+                if (classes[i].equals(classes[j])) {
                     return true;
                 }
             }
+            i++;
         }
         return false;
     }
